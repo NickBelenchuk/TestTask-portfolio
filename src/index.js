ed27a5a9ae1699e4e73c06c6f30981a35c2515d9
Nick <nick.belenchuk@gmail.com>
2023-01-21 14:03:17 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "./index.html";
 import "./index.scss";
+import "./modules/popup";
 import "./modules/cardSort";
 import "./modules/theme";
 import "./modules/swiper";
 import "./modules/scroll";
 import "./modules/range";
-import "./modules/popup";
